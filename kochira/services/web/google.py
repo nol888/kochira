1a6e42db0adc60027781cb6c338b19e42db837ca
Nolan Lum <nolan@nolm.name>
2016-09-02 00:05:30 -0700

    [web/google] add support for image search

--- kochira/services/web/google.py
+++ kochira/services/web/google.py
@@ -26,8 +26,7 @@ def search(ctx, term):
     """
     Google.
 
-    Search for the given terms on Google. If a number is given, it will display
-    that result.
+    Search for the given terms on Google.
     """
 
     r = requests.get(
@@ -44,11 +43,45 @@ def search(ctx, term):
     if not results:
         ctx.respond(ctx._("Couldn't find anything matching \"{term}\".").format(term=term))
         return
-    
+
     total = len(results)
 
     ctx.respond(ctx._("({num} of {total}) {title}: {url}").format(
         title=results[0]["title"],
+        url=results[0]["link"],
+        num=1,
+        total=total
+    ))
+
+@service.command(r"!image (?P<term>.+?)$")
+@service.command(r"image(?: for)? (?P<term>.+?)\??$", mention=True)
+@background
+def image(ctx, term):
+    """
+    Image search.
+
+    Search for the given terms on Google.
+    """
+
+    r = requests.get(
+        "https://www.googleapis.com/customsearch/v1",
+        params={
+            "key": ctx.config.api_key,
+            "cx": ctx.config.cx,
+            "searchType": "image",
+            "q": term
+        }
+    ).json()
+
+    results = r.get("items", [])
+
+    if not results:
+        ctx.respond(ctx._("Couldn't find anything matching \"{term}\".").format(term=term))
+        return
+
+    total = len(results)
+
+    ctx.respond(ctx._("({num} of {total}) {url}").format(
         url=results[0]["link"],
         num=1,
         total=total
